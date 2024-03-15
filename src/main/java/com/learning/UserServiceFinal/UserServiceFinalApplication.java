141c8a0b1b16c8277c86abc490ccc2e8f74d0dcc
stwarHatLuffy <yadraj1803@gmail.com>
2024-03-15 18:07:12 -0400

    Added client discovery eureka -main

--- src/main/java/com/learning/UserServiceFinal/UserServiceFinalApplication.java
+++ src/main/java/com/learning/UserServiceFinal/UserServiceFinalApplication.java
@@ -2,6 +2,7 @@ package com.learning.UserServiceFinal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.CloudEurekaClient;
 
 @SpringBootApplication
 public class UserServiceFinalApplication {
